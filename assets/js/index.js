ffc62027a0b4c415c4cf0b08275d5588db07ac4a
David Jover <david.jover59@gmail.com>
2023-03-01 16:08:30 -0500

    Project finished. Locking works. Time blocks saves data in browser. CSS class changes depending on time. Dynamically adds time blocks. Dynamically styles them.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,83 +1,186 @@
-var text_Hour = 9;
-var hour_after = ":00 am";
+var hourBoxText = 9;
+var hour_Suffix = ":00am";
 
-var storedBlocks = [];
+var blockData = [];
+var blockData_NAME = "Block Data";
 
+function setBGColor($div, currentTime, textTime) {
+  var iTime_CUR = currentTime.split("");
+  var iTime_TXT = textTime.split("");
+
+  if (iTime_CUR[iTime_CUR.length - 2] !== iTime_TXT[iTime_TXT.length - 2]) {
+    if (iTime_CUR[iTime_CUR.length - 2] > iTime_TXT[iTime_TXT.length - 2]) {
+      console.log("p > a");
+      $div.addClass("bg-secondary");
+    } else {
+      console.log("p < a");
+      $div.addClass("bg-primary");
+    }
+  } else {
+    console.log("same time of day");
+
+    var t_CUR = parseHour(iTime_CUR);
+    var t_TXT = parseHour(iTime_TXT);
+
+    if (parseInt(t_CUR) > parseInt(t_TXT)) {
+      console.log("current greater");
+      $div.addClass("bg-secondary");
+    } else if (parseInt(t_CUR) < parseInt(t_TXT)) {
+      if (parseInt(t_TXT) === 12) {
+        console.log("current greater");
+        $div.addClass("bg-secondary");
+      } else {
+        console.log("current less");
+        $div.addClass("bg-primary");
+      }
+    } else {
+      $div.addClass("timecolor");
+    }
+  }
+}
 function spawnBlock(iterations) {
   if (!iterations) {
     iterations = 1;
   }
+
+  var currentTime = GetCurrentHour("LT");
+
   for (var i = 0; i < iterations; i++) {
-    var text_time = text_Hour + hour_after;
+    var text_time = hourBoxText + hour_Suffix;
+
     $iBlock = $("<div>").addClass("row py-1");
+
     $iTimeText = $("<h5>").addClass("text-center").text(text_time);
     $iTimeDiv = $("<div>")
-      .addClass("col-2 py-3 bg-light align-middle")
+      .addClass("col-2 py-3 bg-warning align-middle")
       .append($iTimeText);
-    // Append the "text_time" <h5> element to this new <div>
+
     $iTextDiv = $("<textarea>")
-      .addClass("col-8 py-3 bg-secondary overflow-auto")
+      .addClass("col-8 py-3 overflow-auto")
       .text("")
       .attr("id", text_time);
-    // Set the text of the <textarea> to an empty string
-    // Set the "id" attribute of the <textarea> to "text_time"
+    setBGColor($iTextDiv, currentTime, text_time);
+
     $iLockIcon = $("<span>").addClass("lock");
+
     $iLockDiv = $("<div>")
       .addClass("col-1 py-3 lock-container border border-primary")
       .append($iLockIcon);
 
     $iLockIcon.toggleClass("unlocked");
+
     $iBlock.append($iTimeDiv, $iTextDiv, $iLockDiv);
 
     $("#planner").append($iBlock);
 
-    hourTextIncrease();
+    incrementTextHour();
   }
 }
-// Check if "text_Hour" is equal to 12
-// If it is, set "text_Hour" to 1 and change "hour_after" to ":00 pm"
-// If it's not, increment "text_Hour" by 1
-function hourTextIncrease() {
-  if (text_Hour == 12) {
-    text_Hour = 1;
-    hour_after = ":00 pm";
+// Check if "hour_Text" is equal to 12
+// If it is, set "hour_Text" to 1 and change "hour_after" to ":00 pm"
+// If it's not, increment "hour_Text" by 1
+function incrementTextHour() {
+  if (hourBoxText === 12) {
+    hourBoxText = 1;
+  } else if (hourBoxText === 11) {
+    hour_Suffix = ":00pm";
+    hourBoxText++;
   } else {
-    text_Hour++;
+    hourBoxText++;
   }
 }
-spawnBlock(14);
-DisplayDate("LLLL");
+
 function DisplayDate(pFormat) {
-  // Create a new variable named "date" using the moment.js library to format the current date
   var date = moment().format(pFormat);
+
   $("#current-date").text(date);
-  // Set the text of the element with the ID "current-date" to the "date" variable
 }
-$(".lock").click(function () {
-  $(this).toggleClass("unlocked");
+function GetCurrentHour(pFormat) {
+  var time = moment().format(pFormat).toLowerCase();
 
-  $iTextArea = $($(this).parent().parent().children()[1]);
-  // Get the value of the <textarea> element and create a new variable named "iInput"
-  iInput = $iTextArea.val();
-  // Get the "id" attribute of the <textarea> element and create a new variable named "iID"
+  time = time.split("");
 
-  iID = $iTextArea.attr("id");
-  // Create a new object named "iStoredBlock" with properties "id" and "input" set to the "iID" and "iInput" variables
-  iStoredBlock = {
-    id: iID,
-    input: iInput,
+  var suffix = "";
+
+  var hour = parseHour(time);
+
+  console.log(hour);
+
+  if (time[time.length - 2] === "p") {
+    console.log("afternoon");
+    suffix = ":00pm";
+  } else {
+    console.log("morning");
+    suffix = ":00am";
+  }
+
+  console.log(hour + suffix);
+  return hour + suffix;
+}
+
+function parseHour(pTime) {
+  var i = 0;
+  var iHour = "";
+
+  while (pTime[i] !== ":" || i > 100) {
+    iHour += pTime[i];
+    i++;
+  }
+
+  return iHour;
+}
+
+function AlterStoredBlocks(pText, pID) {
+  nBlock = {
+    id: pID,
+    input: pText.trim(),
   };
-  // Loop through the "storedBlocks" array and remove the object with an "id" property that matches "iID"
-  // If no matching object is found, push the "iStoredBlock" object to the "storedBlocks" array
-  for (var i = 0; i < storedBlocks.length; i++) {
-    if (storedBlocks[i].id === iStoredBlock.id) {
-      storedBlocks.splice(i, 1);
+
+  for (var i = 0; i < blockData.length; i++) {
+    if (blockData[i].id === nBlock.id) {
+      blockData.splice(i, 1);
+
+      localStorage.setItem(blockData_NAME, JSON.stringify(blockData));
 
       return null;
     }
   }
 
-  storedBlocks.push(iStoredBlock);
+  blockData.push(nBlock);
 
-  console.log(storedBlocks);
+  localStorage.setItem(blockData_NAME, JSON.stringify(blockData));
+}
+
+function getData() {
+  if (localStorage.getItem(blockData_NAME)) {
+    blockData = JSON.parse(localStorage.getItem(blockData_NAME));
+
+    blockData.forEach((iBlock) => {
+      iID = "#" + iBlock.id;
+
+      $iBlock = $(document.getElementById(iBlock.id));
+
+      $iBlock.val(iBlock.input);
+
+      $iLock = $($iBlock.parent().children().children()[1]);
+
+      $iLock.toggleClass("unlocked");
+    });
+  }
+}
+spawnBlock(9);
+DisplayDate("LLLL");
+getData();
+
+$(".lock").click(function () {
+  console.log("lock clicked");
+
+  $(this).toggleClass("unlocked");
+
+  $iTextArea = $($(this).parent().parent().children()[1]);
+
+  iInput = $iTextArea.val();
+  iID = $iTextArea.attr("id");
+
+  AlterStoredBlocks(iInput, iID);
 });
